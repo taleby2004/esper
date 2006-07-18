cb2ed0068ad43927787f20b6dffa37d680bc7c91
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2006-07-18 12:42:58 +0000

    use time batch
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@136 d91eb1b3-1215-0410-9718-d41490442091

--- eg/src/main/java/net/esper/example/marketdatafeed/TicksPerSecondStatement.java
+++ eg/src/main/java/net/esper/example/marketdatafeed/TicksPerSecondStatement.java
@@ -11,9 +11,7 @@ public class TicksPerSecondStatement
     public TicksPerSecondStatement(EPAdministrator admin)
     {
         String stmt = "insert into TicksPerSecond (feed, cnt) " +
-                      "select feed, count(*) from MarketDataEvent.win:time(1) group by feed output every 1 seconds";
-
-        //String stmt = "select feed, count(*) as cnt from MarketDataEvent.win:time(1) group by feed output every 1 seconds";
+                      "select feed, count(*) from MarketDataEvent.win:time_batch(1) group by feed";
 
         statement = admin.createEQL(stmt);
     }
