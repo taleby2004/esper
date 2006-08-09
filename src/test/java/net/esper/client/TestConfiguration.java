32c2a4d7ec96fb47f043fd50627228a0db0f23c8
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-09 20:13:27 +0000

    Additional tests for the new map events.
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@194 d91eb1b3-1215-0410-9718-d41490442091

--- src/test/java/net/esper/client/TestConfiguration.java
+++ src/test/java/net/esper/client/TestConfiguration.java
@@ -3,6 +3,8 @@ package net.esper.client;
 import junit.framework.TestCase;
 
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 import java.io.File;
 
 
@@ -11,24 +13,30 @@ public class TestConfiguration extends TestCase
     private final static String SAMPLE_EVENT_NAME = "MySampleEvent";
     private final static String SAMPLE_EVENT_CLASS = "com.mycompany.myapp.MySampleEvent";
     private final static String SAMPLE_IMPORT_PACKAGE = "com.mycompany.myapp.*";
-
+    private final static String SAMPLE_MAP_ALIAS = "mapEvent";
+    private final Map<String, String> typeMap = new HashMap<String, String>();
+    
     private Configuration config;
 
     public void setUp()
     {
         config = new Configuration();
+    	typeMap.put("myString", "String");
+    	typeMap.put("myInt", "Integer");
     }
 
     public void testDefault() throws Exception
     {
         config.configure();
         assertConfig(config, false);
+        assertConfigMap(config);
     }
 
     public void testString() throws Exception
     {
         config.configure(Configuration.ESPER_DEFAULT_CONFIG);
         assertConfig(config, false);
+        assertConfigMap(config);
     }
 
     public void testURL() throws Exception
@@ -36,6 +44,7 @@ public class TestConfiguration extends TestCase
         URL url = this.getClass().getClassLoader().getResource(Configuration.ESPER_DEFAULT_CONFIG);
         config.configure(url);
         assertConfig(config, false);
+        assertConfigMap(config);
     }
 
     public void testFile() throws Exception
@@ -44,16 +53,21 @@ public class TestConfiguration extends TestCase
         File file = new File(url.toURI());
         config.configure(file);
         assertConfig(config, false);
+        assertConfigMap(config);
     }
 
     public void testAddEventMapping()
     {
         config.addEventTypeAlias(SAMPLE_EVENT_NAME, SAMPLE_EVENT_CLASS);
-        assertConfig(config, true);
+    	assertConfig(config, true);
     }
     
+    public void testAddMapAliases()
+    {
+    	config.addMapEvent(SAMPLE_MAP_ALIAS, typeMap);
+    	assertConfigMap(config);
+    }
     
-
     private void assertConfig(Configuration config, boolean isUsingDefaultImports)
     {
         assertEquals(1, config.getEventTypeAliases().size());
@@ -71,6 +85,12 @@ public class TestConfiguration extends TestCase
         {
         	assertEquals(1, config.getImports().size());
         	assertEquals(SAMPLE_IMPORT_PACKAGE, config.getImports().get(0));
-        }
+        }        
+    }
+    
+    private void assertConfigMap(Configuration config)
+    {
+        assertEquals(1, config.getMapAliases().size());
+        assertEquals(typeMap, config.getMapAliases().get(SAMPLE_MAP_ALIAS));
     }
 }
