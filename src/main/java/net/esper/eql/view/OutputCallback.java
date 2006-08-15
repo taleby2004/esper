16ef33ffb2e642aac4cc41503184eddf64984924
srdan <srdan@d91eb1b3-1215-0410-9718-d41490442091>
2006-08-15 16:01:53 +0000

    A new parameter: doOutput.
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@232 d91eb1b3-1215-0410-9718-d41490442091

--- src/main/java/net/esper/eql/view/OutputCallback.java
+++ src/main/java/net/esper/eql/view/OutputCallback.java
@@ -3,13 +3,13 @@ package net.esper.eql.view;
 /**
  * A wrapper for the callback from the output limit condition to the output handler.
  */
-public interface OutputCallback {
-
+public interface OutputCallback 
+{
 	/**
      * Invoked to perform output processing.
-	 * @param forceUpdate is a flag to indicate that even if there is no data, 
-	 * child views should still be updated.
+	 * @param doOutput - true if the batched events should actually be output as well as processed, false if they should just be processed
+	 * @param forceUpdate - true if output should be made even when no updating events have arrived
 	 */
-	public void continueOutputProcessing(boolean forceUpdate);	
+	public void continueOutputProcessing(boolean doOutput, boolean forceUpdate);	
 	
 }
