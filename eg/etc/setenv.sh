e5ec5f75f63b200d0a5ffd3c213c329aee4ce8b0
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2006-10-26 11:29:38 +0000

    Added GPL license removed LGPL; Added a copyright to the public API classes; Added short terminalsvc description to examples section in doc; Bumped release versions
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@449 d91eb1b3-1215-0410-9718-d41490442091

--- eg/etc/setenv.sh
+++ eg/etc/setenv.sh
@@ -20,7 +20,7 @@ LIB=../../lib
 
 CLASSPATH=.
 CLASSPATH=$CLASSPATH:../target/classes
-CLASSPATH=$CLASSPATH:../../esper-1.1.0.jar
+CLASSPATH=$CLASSPATH:../../esper-1.2.0.jar
 CLASSPATH=$CLASSPATH:$LIB/cglib-full-2.0.2.jar
 CLASSPATH=$CLASSPATH:$LIB/commons-beanutils-1.7.0.jar
 CLASSPATH=$CLASSPATH:$LIB/commons-logging-1.0.3.jar
