45873910a66f80370c5ef2bfca39726e01295809
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2006-11-17 00:27:55 +0000

    Removed generated doc files; added adapter doc content; removed IDEA project files
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@494 d91eb1b3-1215-0410-9718-d41490442091

--- esperio/src/test/java/net/esper/regression/adapter/TestCSVAdapterUseCases.java
+++ esperio/src/test/java/net/esper/regression/adapter/TestCSVAdapterUseCases.java
@@ -6,6 +6,8 @@ import net.esper.client.Configuration;
 import net.esper.client.EPStatement;
 import net.esper.adapter.AdapterInputSource;
 import net.esper.adapter.InputAdapter;
+import net.esper.adapter.AdapterCoordinatorImpl;
+import net.esper.adapter.AdapterCoordinator;
 import net.esper.adapter.csv.CSVInputAdapter;
 import net.esper.adapter.csv.CSVInputAdapterSpec;
 import net.esper.support.util.SupportUpdateListener;
@@ -15,11 +17,14 @@ import java.util.Map;
 import java.util.HashMap;
 import java.io.ByteArrayInputStream;
 import java.io.StringReader;
+import java.net.URL;
 
 public class TestCSVAdapterUseCases extends TestCase
 {
     private static String NEW_LINE = System.getProperty("line.separator");
-    private static String CSV_FILENAME = "regression/csvtest_tradedata.csv";
+    private static String CSV_FILENAME_ONELINE_TRADE = "regression/csvtest_tradedata.csv";
+    private static String CSV_FILENAME_TIMESTAMPED_PRICES = "regression/csvtest_timestamp_prices.csv";
+    private static String CSV_FILENAME_TIMESTAMPED_TRADES = "regression/csvtest_timestamp_trades.csv";
 
     private EPServiceProvider epService;
 
@@ -37,7 +42,7 @@ public class TestCSVAdapterUseCases extends TestCase
         SupportUpdateListener listener = new SupportUpdateListener();
         stmt.addListener(listener);
 
-        (new CSVInputAdapter(epService, new AdapterInputSource(CSV_FILENAME), "TypeA")).start();
+        (new CSVInputAdapter(epService, new AdapterInputSource(CSV_FILENAME_ONELINE_TRADE), "TypeA")).start();
 
         assertEquals(1, listener.getNewDataList().size());
     }
@@ -74,7 +79,7 @@ public class TestCSVAdapterUseCases extends TestCase
         SupportUpdateListener listener = new SupportUpdateListener();
         stmt.addListener(listener);
 
-        CSVInputAdapterSpec spec = new CSVInputAdapterSpec(new AdapterInputSource(CSV_FILENAME), "TypeA");
+        CSVInputAdapterSpec spec = new CSVInputAdapterSpec(new AdapterInputSource(CSV_FILENAME_ONELINE_TRADE), "TypeA");
         spec.setEventsPerSec(1000);
 //        spec.setLooping(true);
         spec.setUsingEngineThread(true);
@@ -99,7 +104,7 @@ public class TestCSVAdapterUseCases extends TestCase
         SupportUpdateListener listener = new SupportUpdateListener();
         stmt.addListener(listener);
 
-        CSVInputAdapterSpec spec = new CSVInputAdapterSpec(new AdapterInputSource(CSV_FILENAME), "TypeA");
+        CSVInputAdapterSpec spec = new CSVInputAdapterSpec(new AdapterInputSource(CSV_FILENAME_ONELINE_TRADE), "TypeA");
         spec.setEventsPerSec(1000);
 
         InputAdapter inputAdapter = new CSVInputAdapter(epService, spec);
@@ -113,7 +118,7 @@ public class TestCSVAdapterUseCases extends TestCase
      */
     public void testDynamicType()
     {
-        CSVInputAdapterSpec spec = new CSVInputAdapterSpec(new AdapterInputSource(CSV_FILENAME), "TypeB");
+        CSVInputAdapterSpec spec = new CSVInputAdapterSpec(new AdapterInputSource(CSV_FILENAME_ONELINE_TRADE), "TypeB");
 
         epService = EPServiceProviderManager.getDefaultProvider();
         epService.initialize();
@@ -129,6 +134,45 @@ public class TestCSVAdapterUseCases extends TestCase
         assertEquals(String.class, stmt.getEventType().getPropertyType("volume"));
 
         feed.start();
+        assertEquals(1, listener.getNewDataList().size());
+    }
+
+    public void testCoordinated() throws Exception
+    {
+        Map<String, Class> priceProps = new HashMap<String, Class>();
+        priceProps.put("timestamp", Long.class);
+        priceProps.put("symbol", String.class);
+        priceProps.put("price", Double.class);
+
+        Map<String, Class> tradeProps = new HashMap<String, Class>();
+        tradeProps.put("timestamp", Long.class);
+        tradeProps.put("symbol", String.class);
+        tradeProps.put("notional", Double.class);
+
+        epService = EPServiceProviderManager.getProvider("testCoordinated");
+        epService.initialize();
+
+        AdapterInputSource sourceOne = new AdapterInputSource(CSV_FILENAME_TIMESTAMPED_PRICES);
+        CSVInputAdapterSpec inputOneSpec = new CSVInputAdapterSpec(sourceOne, "PriceEvent");
+        inputOneSpec.setTimestampColumn("timestamp");
+        inputOneSpec.setPropertyTypes(priceProps);
+        CSVInputAdapter inputOne = new CSVInputAdapter(inputOneSpec);
+
+        AdapterInputSource sourceTwo = new AdapterInputSource(CSV_FILENAME_TIMESTAMPED_TRADES);
+        CSVInputAdapterSpec inputTwoSpec = new CSVInputAdapterSpec(sourceTwo, "TradeEvent");
+        inputTwoSpec.setTimestampColumn("timestamp");
+        inputTwoSpec.setPropertyTypes(tradeProps);
+        CSVInputAdapter inputTwo = new CSVInputAdapter(inputTwoSpec);
+
+        EPStatement stmt = epService.getEPAdministrator().createEQL("select symbol, price, volume from TradeEvent.win:length(100)");
+        SupportUpdateListener listener = new SupportUpdateListener();
+        stmt.addListener(listener);
+
+        AdapterCoordinator coordinator = new AdapterCoordinatorImpl(epService, true);
+        coordinator.coordinate(inputOne);
+        coordinator.coordinate(inputTwo);
+        coordinator.start();
+
         assertEquals(1, listener.getNewDataList().size());
     }
 
