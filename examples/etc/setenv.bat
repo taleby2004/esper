36a2aebb823a40242540ca38c4a183ab37f28ffb
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2007-04-22 00:35:47 +0000

    bumped version to 1.8
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@744 d91eb1b3-1215-0410-9718-d41490442091

--- examples/etc/setenv.bat
+++ examples/etc/setenv.bat
@@ -14,7 +14,7 @@ if not exist "%JAVA_HOME%\bin\java.exe" (
 
 set LIB=..\..\esper\lib
 
-if not exist ..\..\esper-1.7.0.jar goto badenv_esperlib
+if not exist ..\..\esper-1.8.0.jar goto badenv_esperlib
 if not exist %LIB%\cglib-full-2.0.2.jar goto badenv
 if not exist %LIB%\commons-logging-1.0.3.jar goto badenv
 if not exist %LIB%\log4j-1.2.8.jar goto badenv
@@ -22,7 +22,7 @@ if not exist %LIB%\antlr-2.7.5.jar goto badenv
 
 set CLASSPATH=.
 set CLASSPATH=%CLASSPATH%;..\target\classes
-set CLASSPATH=%CLASSPATH%;..\..\esper-1.7.0.jar
+set CLASSPATH=%CLASSPATH%;..\..\esper-1.8.0.jar
 set CLASSPATH=%CLASSPATH%;%LIB%\cglib-full-2.0.2.jar
 set CLASSPATH=%CLASSPATH%;%LIB%\commons-logging-1.0.3.jar
 set CLASSPATH=%CLASSPATH%;%LIB%\log4j-1.2.8.jar
--- examples/etc/setenv.sh
+++ examples/etc/setenv.sh
@@ -20,7 +20,7 @@ LIB=../../esper/lib
 
 CLASSPATH=.
 CLASSPATH=$CLASSPATH:../target/classes
-CLASSPATH=$CLASSPATH:../../esper-1.7.0.jar
+CLASSPATH=$CLASSPATH:../../esper-1.8.0.jar
 CLASSPATH=$CLASSPATH:$LIB/cglib-full-2.0.2.jar
 CLASSPATH=$CLASSPATH:$LIB/commons-logging-1.0.3.jar
 CLASSPATH=$CLASSPATH:$LIB/log4j-1.2.8.jar
