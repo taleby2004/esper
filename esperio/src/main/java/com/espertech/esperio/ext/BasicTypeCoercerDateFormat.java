2c39d3452e506e610ba94e4b626abd1c7ee1f611
bernhardttom <bernhardttom@d91eb1b3-1215-0410-9718-d41490442091>
2008-08-07 12:22:48 +0000

    Minor patch apply fix; Esper-267 Allow SenderThreadPool to be constructed with an ExecutorService for EsperIO sender
    git-svn-id: http://svn.codehaus.org/esper/esper/trunk@1391 d91eb1b3-1215-0410-9718-d41490442091

--- esperio/src/main/java/com/espertech/esperio/ext/BasicTypeCoercerDateFormat.java
+++ esperio/src/main/java/com/espertech/esperio/ext/BasicTypeCoercerDateFormat.java
@@ -23,28 +23,3 @@ public class BasicTypeCoercerDateFormat extends BasicTypeCoercer {
 		this.dateFormats.put(propertyName, df);
 	}
 }
-package com.espertech.esperio.ext;
-
-import java.text.DateFormat;
-import java.util.HashMap;
-import java.util.Map;
-
-import com.espertech.esperio.csv.BasicTypeCoercer;
-
-public class BasicTypeCoercerDateFormat extends BasicTypeCoercer {
-
-	private Map<String, DateFormat> dateFormats = new HashMap<String, DateFormat>();
-
-	@Override
-	public Object coerce(String property, String source) throws Exception {
-		DateFormat df = dateFormats.get(property);
-		if (df != null) {
-			return df.parse(source).getTime();
-		}
-		return super.coerce(property, source);
-	}
-	
-	public void addDateFormat(String propertyName, DateFormat df) {
-		this.dateFormats.put(propertyName, df);
-	}
-}
--- esperio/src/main/java/com/espertech/esperio/ext/SenderThreadPool.java
+++ esperio/src/main/java/com/espertech/esperio/ext/SenderThreadPool.java
@@ -17,44 +17,12 @@ public class SenderThreadPool extends DirectSender {
 	public SenderThreadPool(int threadPoolSize) {
 		executorService = Executors.newFixedThreadPool(threadPoolSize);
 	}
-	
-	public void sendEvent(final Object beanToSend) {
-		executorService.execute(new Runnable() {
-			public void run() {
-				runtime.sendEvent(beanToSend);
-			}
-		});
-	}
 
-	public void sendEvent(final Map mapToSend, final String eventTypeAlias) {
-		executorService.execute(new Runnable() {
-			public void run() {
-				runtime.sendEvent(mapToSend, eventTypeAlias);
-			}
-		});
-	}
-}
-package com.espertech.esperio.ext;
+    public SenderThreadPool(ExecutorService executorService) {
+        this.executorService = executorService;
+    }
 
-import java.util.Map;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-import com.espertech.esperio.DirectSender;
-
-/**
- * Implementation of Sender to send to esper engine using a thread pool
- * @author Jerry Shea
- */
-public class SenderThreadPool extends DirectSender {
-
-	private ExecutorService executorService;
-
-	public SenderThreadPool(int threadPoolSize) {
-		executorService = Executors.newFixedThreadPool(threadPoolSize);
-	}
-	
-	public void sendEvent(final Object beanToSend) {
+    public void sendEvent(final Object beanToSend) {
 		executorService.execute(new Runnable() {
 			public void run() {
 				runtime.sendEvent(beanToSend);
